924c1df80a0a034df910b1e02ff4b8b3a372c93a
MatanGabay <matan.gabay@kellogg.northwestern.edu>
2020-06-04 23:46:49 +0000

    password

--- app.rb
+++ app.rb
@@ -33,14 +33,33 @@ get "/" do
     view "restaurants"
 end
 
-# Show a single event
+# Show a single restaurant
 get "/restaurants/:id" do
     @users_table = users_table
     # SELECT * FROM events WHERE id=:id
     @restaurant = restaurant_table.where(:id => params["id"]).to_a[0]
     # SELECT * FROM rsvps WHERE event_id=:id
     @reviews= reviews_table.where(:restaurant_id => params["id"]).to_a
+    
+    @current_user = users_table.where(:id => session[:user_id]).to_a[0]
+    puts @current_user.inspect
+
     view "restaurant"
+end
+
+# Form to create a new Review
+get "/restaurants/:id/reviews/new" do
+    @restaurant = restaurant_table.where(:id => params["id"]).to_a[0]
+    view "new_review"
+end
+
+# Receiving end of new Review form
+post "/restaurants/:id/reviews/create" do
+    reviews_table.insert(:restaurant_id => params["id"],
+                       :user_id => @current_user[:id],
+                       :comments => params["comments"])
+    @restaurant = reviews_table.where(:id => params["id"]).to_a[0]
+    view "create_review"
 end
 
 # Form to create a new user
@@ -52,7 +71,7 @@ end
 post "/users/create" do
     users_table.insert(:name => params["name"],
                        :email => params["email"],
-                       :password => params["password"])
+                       :password => BCrypt::Password.create(params["password"]))
     view "create_user"
 end
 
@@ -71,7 +90,7 @@ post "/logins/create" do
     if user
         puts user.inspect
         # test the password against the one in the users table
-        if user[:password] == password_entered
+        if  BCrypt::Password.new(user[:password]) == password_entered
             session[:user_id] = user[:id]
             view "create_login"
         else
@@ -84,5 +103,6 @@ end
 
 # Logout
 get "/logout" do
+    session[:user_id] = nil
     view "logout"
 end
